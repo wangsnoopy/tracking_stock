9da3e9cde7e911b6a0d896c01fc22e5a465189a7
Austin Wang <awang@Austins-Virtual-Machine.local>
2025-06-01 16:38:36 -0700

    fix the date issue

--- optimize_something/optimization.py
+++ optimize_something/optimization.py
@@ -125,9 +125,9 @@ def test_code():
     This function WILL NOT be called by the auto grader.  		  	   		 	 	 			  		 			 	 	 		 		 	
     """  		  	   		 	 	 			  		 			 	 	 		 		 	
   		  	   		 	 	 			  		 			 	 	 		 		 	
-    start_date = dt.datetime(2009, 1, 1)  		  	   		 	 	 			  		 			 	 	 		 		 	
-    end_date = dt.datetime(2010, 1, 1)  		  	   		 	 	 			  		 			 	 	 		 		 	
-    symbols = ["GOOG", "AAPL", "GLD", "XOM", "IBM"]  		  	   		 	 	 			  		 			 	 	 		 		 	
+    start_date = dt.datetime(2008, 6, 1)
+    end_date = dt.datetime(2009, 6, 1)
+    symbols = ["IBM", "X", "GLD", "JPM"]
   		  	   		 	 	 			  		 			 	 	 		 		 	
     # Assess the portfolio  		  	   		 	 	 			  		 			 	 	 		 		 	
     allocations, cr, adr, sddr, sr = optimize_portfolio(  		  	   		 	 	 			  		 			 	 	 		 		 	
